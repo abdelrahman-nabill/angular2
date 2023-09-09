33e10a976dcf8ebe0a2ed8213ba627b2f54358ee
unknown <abdelrahmaan.naabil@gmail.com>
2023-09-09 17:26:09 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { SubfooterComponent } from './subfooter/subfooter.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    HomeComponent,
+    FooterComponent,
+    SubfooterComponent,
+
   ],
   imports: [
     BrowserModule,
